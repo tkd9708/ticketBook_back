cd9639d46f356d3487a7cc20828c4136a88a7d72
tkd9708 <tkd9708@naver.com>
2021-04-06 23:44:56 +0900

    bookdto 생성

---
+++ src/main/java/com/ticket/dto/BookDto.java
@@ -0,0 +1,65 @@
+package com.ticket.dto;
+
+import java.util.Date;
+
+public class BookDto {
+	private String _id;
+	private String memId;
+	private String bookTitle;
+	private String photo;
+	private String location;
+	private Date readDate;
+	private int star;
+	private String review;
+	
+	
+	public String get_id() {
+		return _id;
+	}
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+	public String getMemId() {
+		return memId;
+	}
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+	public String getBookTitle() {
+		return bookTitle;
+	}
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public Date getReadDate() {
+		return readDate;
+	}
+	public void setReadDate(Date readDate) {
+		this.readDate = readDate;
+	}
+	public int getStar() {
+		return star;
+	}
+	public void setStar(int star) {
+		this.star = star;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	
+}
